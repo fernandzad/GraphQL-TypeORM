a0fc3578a575db16d97568a27f2721ff45e677cf
AdFer25 <adrian.fernandez@wizeline.com>
2020-06-11 09:09:20 -0500

    Few changes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,8 @@ export const startServer = async () => {
 
   const server = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [PingResolver, ProductResolver]
+      resolvers: [PingResolver, ProductResolver],
+      validate: false,
     }),
     context: ({req, res}) => ({req, res})
   })
